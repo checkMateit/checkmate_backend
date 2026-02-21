2372380d42aedd7545dfc8e2790fce9d5447b920
Logan <jjk1491@gmail.com>
2026-02-21 17:31:02 +0900

    feat: Implement RabbitMQ retry policy and Dead Letter Queue #35

--- store-service/src/main/java/com/checkit/storeservice/notificationservice/config/RabbitConfig.java
+++ store-service/src/main/java/com/checkit/storeservice/notificationservice/config/RabbitConfig.java
@@ -1,9 +1,6 @@
 package com.checkit.storeservice.notificationservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -18,9 +15,15 @@ public class RabbitConfig {
     public static final String EXCHANGE_NAME = "notification.exchange";
     public static final String ROUTING_KEY = "notification.key";
 
+    public static final String DLX_NAME = "notification.dlx";
+    public static final String DLQ_NAME = "notification.dlq";
+
     @Bean
     public Queue notificationQueue() {
-        return new Queue(QUEUE_NAME, true);
+        return QueueBuilder.durable(QUEUE_NAME)
+                .withArgument("x-dead-letter-exchange", DLX_NAME)
+                .withArgument("x-dead-letter-routing-key", "deadletter")
+                .build();
     }
 
     @Bean
@@ -44,4 +47,20 @@ public class RabbitConfig {
         rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
         return rabbitTemplate;
     }
+
+    @Bean
+    public DirectExchange deadLetterExchange() {
+        return new DirectExchange(DLX_NAME);
+    }
+
+    @Bean
+    public Queue deadLetterQueue() {
+        return new Queue(DLQ_NAME, true);
+    }
+
+    @Bean
+    public Binding dlqBinding() {
+        return BindingBuilder.bind(deadLetterQueue()).to(deadLetterExchange()).with("deadletter");
+    }
+
 }
