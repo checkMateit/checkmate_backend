8c480818e911af9af3088b541fd2f3805d436abd
hystrix96 <leo8071004@naver.com>
2026-03-01 19:27:35 +0900

    feat: Add authorUserId to board post list API spec

--- study-service/src/main/java/com/checkit/studyservice/dto/BoardPostListItemRes.java
+++ study-service/src/main/java/com/checkit/studyservice/dto/BoardPostListItemRes.java
@@ -14,6 +14,7 @@ public class BoardPostListItemRes {
 
     private Long postId;
     private String title;
+    private UUID authorUserId;
     private long empathyCount;
     private long commentCount;
 }
